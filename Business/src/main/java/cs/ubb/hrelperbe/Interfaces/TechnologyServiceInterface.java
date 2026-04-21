74e4dac00b4896fb944dc42328e0ebb76ccf1b7f
Armand-Valentin Brudea <147134068+Minimatrix26@users.noreply.github.com>
2026-04-21 19:25:33 +0300

    added get all technologies endpoint (#23)

---
+++ Business/src/main/java/cs/ubb/hrelperbe/Interfaces/TechnologyServiceInterface.java
@@ -0,0 +1,9 @@
+package cs.ubb.hrelperbe.Interfaces;
+
+import cs.ubb.hrelperbe.DTOs.TechnologyResponse;
+
+import java.util.List;
+
+public interface TechnologyServiceInterface {
+    List<TechnologyResponse> getAllTechnologies();
+}
