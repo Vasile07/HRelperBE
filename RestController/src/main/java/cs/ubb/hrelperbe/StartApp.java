1912f4e3024085e51e42bcb0ad41cb685e586e43
MihaiDer <mihaider10@gmail.com>
2026-04-24 12:16:45 +0300

    Add GET /roles endpoint with department data

--- RestController/src/main/java/cs/ubb/hrelperbe/StartApp.java
+++ RestController/src/main/java/cs/ubb/hrelperbe/StartApp.java
@@ -2,8 +2,10 @@ package cs.ubb.hrelperbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = {"classpath:application.properties", "file:./RestController/src/main/resources/application.properties"}, ignoreResourceNotFound = true)
 public class StartApp {
     public static void main(String[] args) {
         SpringApplication.run(StartApp.class, args);
