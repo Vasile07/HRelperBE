5c855e7393bf8d606f717a71c55f406a5b448f5b
MihaiDer <mihaider10@gmail.com>
2026-04-24 16:43:03 +0300

    Add GET /users/me endpoint for logged-in user details

--- Business/src/main/java/cs/ubb/hrelperbe/Interfaces/UserServiceInterface.java
+++ Business/src/main/java/cs/ubb/hrelperbe/Interfaces/UserServiceInterface.java
@@ -2,9 +2,12 @@ package cs.ubb.hrelperbe.Interfaces;
 
 import cs.ubb.hrelperbe.DTOs.AccountRegistrationData;
 import cs.ubb.hrelperbe.DTOs.LoginCredentials;
+import cs.ubb.hrelperbe.DTOs.UserDetailsData;
 
 public interface UserServiceInterface {
     public String login(LoginCredentials loginCredentials);
 
     public void register(AccountRegistrationData accountRegistrationData);
+
+    public UserDetailsData getUserDetails(Integer userId);
 }
